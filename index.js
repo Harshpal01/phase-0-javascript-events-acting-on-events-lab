eba2436f669e601b28963665276d055746f557bc
Dominic Kipkorir <harsh979pal@gamil.com>
2025-03-04 10:39:59 +0300

    completed dodgergame

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) { 
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const maxRight = 360;
+
+  if (left < maxRight) { 
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
